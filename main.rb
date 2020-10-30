862524db665416cbef61670fb73e9d8a29a5a6d8
Naqhid <mnaqhid@gmail.com>
2020-10-30 14:55:34 +0530

    sort next array

--- main.rb
+++ main.rb
@@ -10,3 +10,23 @@ def bubble_sort (arr)
   arr
 end  
 p bubble_sort([3, 1, 2, 4, 10, 9, 7, 8, 5, 6])
+
+
+
+def bubble_sort_by(arr)
+  arr.each_with_index do |_value, _i|
+    arr.each_with_index do |_val, index|
+      break if index.next == arr.length
+
+      difference = yield(arr[index], arr[index.next])
+      difference.positive? && (arr[index], arr[index.next] = arr[index.next], arr[index])
+    end
+  end
+  arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
+
+p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
